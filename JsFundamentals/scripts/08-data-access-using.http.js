472e131b0d25d8403ec0ad73c15e8a7879c1a9e5
Jossan Petrenko <jossanpetrenko@hotmail.com>
2019-09-18 18:27:41 -0400

    Data Access Using HTTP

--- JsFundamentals/scripts/08-data-access-using.http.js
+++ JsFundamentals/scripts/08-data-access-using.http.js
@@ -1,21 +1,57 @@
 
-//settingAPromise();
-function settingAPromise() {
+//hTTPRequestUsingXHR();
+function hTTPRequestUsingXHR() {
+    //XML HTTP Request
 
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            console.log(this.responseText);
+        }
+    };
 
-    console.log("Setting a Promise");
+    xhttp.open("GET", "http://5d83fa06baffda001476ac60.mockapi.io/users", true);
+    xhttp.send();
+
+    console.log("HTTP Request Using XHR");
 }
 
-//settingAPromise();
-function settingAPromise() {
+//hTTPRequestUsingJQuery();
+function hTTPRequestUsingJQuery() {
 
+    // yarn add jquery
+    // import $ from 'jquery';
 
-    console.log("Setting a Promise");
+    // $.get("http://5d83fa06baffda001476ac60.mockapi.io/users",
+    //     data => console.log('data :', data)
+    // ); //returns a promise
+
+    let promise = $.get("http://5d83fa06baffda001476ac60.mockapi.io/users");
+    promise.then(
+        data => console.log('data :', data),
+        error => console.log('error :', error)
+    );
+
+    console.log("HTTP Request Using JQuery");
 }
 
-//settingAPromise();
-function settingAPromise() {
+//hTTPPostUsingJQuery();
+function hTTPPostUsingJQuery() {
+
+    // yarn add jquery
+    // import $ from 'jquery';
+
+    let user = {
+        name: 'Citizen J',
+        avater: 'cj.jpg'
+    };
+    let promise = $.post("http://5d83fa06baffda001476ac60.mockapi.io/users", user);
+
+    promise.then(
+        data => console.log('data :', data),
+        error => console.log('error :', error)
+    );
 
 
-    console.log("Setting a Promise");
+    console.log("HTTP Post Using JQuery");
 }
---
+++ JsFundamentals/scripts/09-forms.js
@@ -0,0 +1,23 @@
+
+//hTTPPostUsingJQuery();
+function hTTPPostUsingJQuery() {
+
+
+    console.log("HTTP Post Using JQuery");
+}
+
+
+//hTTPPostUsingJQuery();
+function hTTPPostUsingJQuery() {
+
+
+    console.log("HTTP Post Using JQuery");
+}
+
+
+//hTTPPostUsingJQuery();
+function hTTPPostUsingJQuery() {
+
+
+    console.log("HTTP Post Using JQuery");
+}
